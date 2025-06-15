83da32af537e7115ada3c4b50abce53bfadda5be
Himanchal singh <139227986+himanchalsingh@users.noreply.github.com>
2025-06-15 20:48:52 +0530

    change many things

--- api/app.js
+++ api/app.js
@@ -7,9 +7,10 @@ import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
+import dotenv from "dotenv";
 
 const app = express();
-
+dotenv.config();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
