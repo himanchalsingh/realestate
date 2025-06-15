d766fbcc0b96f0d5068c84d8847a316be6aa1e4f
Himanchal singh <139227986+himanchalsingh@users.noreply.github.com>
2025-06-16 01:48:09 +0530

    add

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("https://realestate-3-4vim.onrender.com"));
+    setSocket(io("http://localhost:8800"));
   }, []);
 
   useEffect(() => {
--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://realestate-1-g882.onrender.com",
+  baseURL: "http://localhost:8800",
   withCredentials: true,
 });
 
