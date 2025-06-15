021cb5b78e9d602521023ee6cfed6ee924951b12
Himanchal singh <139227986+himanchalsingh@users.noreply.github.com>
2025-06-16 01:25:47 +0530

    add backend new url

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("https://realestate-li1s.onrender.com"));
+    setSocket(io("https://realestate-2-c2nw.onrender.com"));
   }, []);
 
   useEffect(() => {
--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://realestate-api-sad8.onrender.com",
+  baseURL: "https://realestate-1-g882.onrender.com",
   withCredentials: true,
 });
 
